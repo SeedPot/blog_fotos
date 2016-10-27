ff107099d5c6f290c786e30d5d3e581ab30072c8
Anthelios <julio.caruncho.a@gmail.com>
2016-10-27 00:34:13 -0300

    QUERYS

--- app/routes/fotos/blog.php
+++ app/routes/fotos/blog.php
@@ -3,12 +3,40 @@
 $app->get('/blog', function($request, $response, $args){
 
 	try {
-		$query = $this->db->prepare("	SELECT * 
-										FROM publicacion 
-										ORDER BY id_publicacion DESC, fecha_publicacion DESC");
+		$query = $this->db->prepare("	SELECT 	PU.ID_PUBLICACION,
+												PU.TITULO_PUBLICACION,
+												PU.LUGAR_PUBLICACION,
+												date_format(PU.FECHA_PUBLICACION,'%d/%m/%Y') as FECHA_PUBLICACION,
+												PU.DESCRIPCION_PUBLICACION,
+												PU.VISITAS_PUBLICACION,
+												(SELECT COUNT(*)
+												FROM	COMENTARIO CO
+												WHERE	PU.ID_PUBLICACION = CO.ID_PUBLICACION) as NUMERO_COMENTARIOS
+										FROM 	PUBLICACION PU 
+										ORDER BY ID_PUBLICACION DESC, FECHA_PUBLICACION DESC");
 		$query->execute();
 		$posts = $query->fetchAll();
+		for ($i=0; $i < count($posts); $i++){
+			$query = $this->db->prepare("	SELECT 	PU.ID_PUBLICACION,
+													ET.ID_ETIQUETA,
+													ET.NOMBRE_ETIQUETA
+											FROM 	ETIQUETA_PUBLICACION PU, ETIQUETA ET
+											WHERE	ET.ID_ETIQUETA = PU.ID_ETIQUETA
+											AND 	PU.ID_PUBLICACION = :id_publicacion
+											ORDER BY PU.ID_PUBLICACION ASC, ET.ID_ETIQUETA ASC");
+			$query->bindParam(':id_publicacion', $posts[$i]['ID_PUBLICACION']);
+			$query->execute();
+			$tags = $query->fetchAll();
+			$posts[$i]['tags']=array();
+			foreach ($tags as $tag) {
+				array_push($posts[$i]['tags'], $tag);
+			}
+		}		
 		$this->db = null;
+		/*echo('<pre>');
+		var_dump($posts);
+		echo('</pre>');*/
+		//$cosas = array('post' => $posts,'hola '=> $hola );
 
 	} catch(PDOException $e) {
 		echo "Error: " . $e->getMessage();
@@ -19,4 +47,25 @@ $app->get('/blog', function($request, $response, $args){
 
 })->setName('blog');
 
+$app->get('/post/{post}', function($request, $response, $args){
+	$post = $args['post'];
+	try {
+		$query = $this->db->prepare("	SELECT 	PU.ID_PUBLICACION,
+												PU.TITULO_PUBLICACION,
+												PU.LUGAR_PUBLICACION,
+												date_format(PU.FECHA_PUBLICACION,'%d/%m/%Y') as FECHA_PUBLICACION,
+												PU.DESCRIPCION_PUBLICACION,
+												PU.VISITAS_PUBLICACION
+										FROM 	PUBLICACION PU 
+										WHERE	PU.ID_PUBLICACION = :id_publicacion
+										ORDER BY ID_PUBLICACION DESC, FECHA_PUBLICACION DESC");
+		$query->bindParam(':id_publicacion', $post);
+		$query->execute();
+		$post = $query->fetch();
+	} catch(PDOException $e) {
+		echo "Error: " . $e->getMessage();
+	}
+	
+	return $this->view->render($response, 'fotos/post.html', ['post' => $post]);
 
+})->setName('post');
